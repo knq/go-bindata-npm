f19a1e1c4950d247f773808bb50b0af10b3e11ee
Kenneth Shaw <kenneth.shaw@knq.io>
2017-07-06 12:01:35 +0700

    Adding windows binary, bin/go-bindata script, and bin entry to package.json

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ PACKAGE=github.com/jteeuwen/go-bindata/go-bindata
 
 go get -u $PACKAGE
 
-for PLATFORM in linux darwin; do
-  GOOS=$PLATFORM GOARCH=amd64 go build -o vendor/go-bindata-$PLATFORM $PACKAGE
+for PLATFORM in linux darwin windows; do
+  EXT=""
+  if [ "$PLATFORM" = "windows" ]; then
+    EXT=".exe"
+  fi
+  GOOS=$PLATFORM GOARCH=amd64 go build -o vendor/go-bindata-$PLATFORM$EXT $PACKAGE
 done
